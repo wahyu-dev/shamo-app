1857165f2f7f746d2e45517a604f9d60db8a6c17
wahyuwibowo <wahyuwibowo@mdd.co.id>
2021-08-19 22:09:48 +0700

    slicing sign in page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:shamo_app_flutter/theme.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/sign-in'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage('assets/splash_icon.png'))),
+        ),
+      ),
+    );
+  }
+}
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 Color primaryColor = Color(0xff6C5ECF);
 Color secondaryColor = Color(0xff38ABBE);
 Color alertColor = Color(0xffED6363);
@@ -10,18 +12,15 @@ Color bgColor2 = Color(0xff2B2937);
 Color bgColor3 = Color(0xff242231);
 Color primaryTextColor = Color(0xffE1E1E1);
 Color secondaryTextColor = Color(0xff999999);
+Color subtitleColor = Color(0xff504f5e);
 
-TextStyle primaryTextStyle = GoogleFonts.poppins(
-  color: primaryTextColor
-);
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
 
-TextStyle secondaryTextStyle = GoogleFonts.poppins(
-    color: secondaryTextColor
-);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryTextColor);
 
-TextStyle priceTextStyle = GoogleFonts.poppins(
-    color: priceColor
-);
+TextStyle priceTextStyle = GoogleFonts.poppins(color: priceColor);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(color: subtitleColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: primaryColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
